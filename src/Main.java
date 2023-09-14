47421b49e61133f44a974f74df51c6f52a0e5b5d
HUDAAYOUB <huda3ya@gmail.com>
2023-09-14 18:20:47 +0300

    Q1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int Max [] = {5,3,7,4,71,3,78,1,66,78,53,8,13};
+        int largest = Max[0];
+        for (int i =0; i <= 12; i++){
+            if (Max[i] > largest) largest = Max[i];}
+        System.out.println("The Maximum Number is " + largest);
+
+
+
+
+    }}
+
